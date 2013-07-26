f4ccaaba0f76f30cd03dea53a73ff544976c789e
Daniel Margolis <dmargolis@mkoob.prod.google.com>
2013-07-26 22:16:06 +0200

    Save a CRC of the master password+salt to synced storage to catch typoes.

--- popup.js
+++ popup.js
@@ -1,431 +1,108 @@
-/*
- * Crypto-JS v2.0.0
- * http://code.google.com/p/crypto-js/
- * Copyright (c) 2009, Jeff Mott. All rights reserved.
- * http://code.google.com/p/crypto-js/wiki/License
- */
-
-
-/* Crypto */
-
-if (typeof Crypto == "undefined" || ! Crypto.util)
-{
-(function(){
-
-var base64map = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-// Global Crypto object
-var Crypto = window.Crypto = {};
-
-// Crypto utilities
-var util = Crypto.util = {
-
-	// Bit-wise rotate left
-	rotl: function (n, b) {
-		return (n << b) | (n >>> (32 - b));
-	},
-
-	// Bit-wise rotate right
-	rotr: function (n, b) {
-		return (n << (32 - b)) | (n >>> b);
-	},
-
-	// Swap big-endian to little-endian and vice versa
-	endian: function (n) {
-
-		// If number given, swap endian
-		if (n.constructor == Number) {
-			return util.rotl(n,  8) & 0x00FF00FF |
-			       util.rotl(n, 24) & 0xFF00FF00;
-		}
-
-		// Else, assume array and swap all items
-		for (var i = 0; i < n.length; i++)
-			n[i] = util.endian(n[i]);
-		return n;
-
-	},
-
-	// Generate an array of any length of random bytes
-	randomBytes: function (n) {
-		for (var bytes = []; n > 0; n--)
-			bytes.push(Math.floor(Math.random() * 256));
-		return bytes;
-	},
-
-	// Convert a byte array to big-endian 32-bit words
-	bytesToWords: function (bytes) {
-		for (var words = [], i = 0, b = 0; i < bytes.length; i++, b += 8)
-			words[b >>> 5] |= bytes[i] << (24 - b % 32);
-		return words;
-	},
-
-	// Convert big-endian 32-bit words to a byte array
-	wordsToBytes: function (words) {
-		for (var bytes = [], b = 0; b < words.length * 32; b += 8)
-			bytes.push((words[b >>> 5] >>> (24 - b % 32)) & 0xFF);
-		return bytes;
-	},
-
-	// Convert a byte array to a hex string
-	bytesToHex: function (bytes) {
-		for (var hex = [], i = 0; i < bytes.length; i++) {
-			hex.push((bytes[i] >>> 4).toString(16));
-			hex.push((bytes[i] & 0xF).toString(16));
-		}
-		return hex.join("");
-	},
-
-	// Convert a hex string to a byte array
-	hexToBytes: function (hex) {
-		for (var bytes = [], c = 0; c < hex.length; c += 2)
-			bytes.push(parseInt(hex.substr(c, 2), 16));
-		return bytes;
-	},
-
-	// Convert a byte array to a base-64 string
-	bytesToBase64: function (bytes) {
-
-		// Use browser-native function if it exists
-		if (typeof btoa == "function") return btoa(Binary.bytesToString(bytes));
-
-		for(var base64 = [], i = 0; i < bytes.length; i += 3) {
-			var triplet = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
-			for (var j = 0; j < 4; j++) {
-				if (i * 8 + j * 6 <= bytes.length * 8)
-					base64.push(base64map.charAt((triplet >>> 6 * (3 - j)) & 0x3F));
-				else base64.push("=");
-			}
-		}
-
-		return base64.join("");
-
-	},
-
-	// Convert a base-64 string to a byte array
-	base64ToBytes: function (base64) {
-
-		// Use browser-native function if it exists
-		if (typeof atob == "function") return Binary.stringToBytes(atob(base64));
-
-		// Remove non-base-64 characters
-		base64 = base64.replace(/[^A-Z0-9+\/]/ig, "");
-
-		for (var bytes = [], i = 0, imod4 = 0; i < base64.length; imod4 = ++i % 4) {
-			if (imod4 == 0) continue;
-			bytes.push(((base64map.indexOf(base64.charAt(i - 1)) & (Math.pow(2, -2 * imod4 + 8) - 1)) << (imod4 * 2)) |
-			           (base64map.indexOf(base64.charAt(i)) >>> (6 - imod4 * 2)));
-		}
-
-		return bytes;
-
-	}
-
-};
-
-// Crypto mode namespace
-Crypto.mode = {};
-
-// Crypto character encodings
-var charenc = Crypto.charenc = {};
-
-// UTF-8 encoding
-var UTF8 = charenc.UTF8 = {
-
-	// Convert a string to a byte array
-	stringToBytes: function (str) {
-		return Binary.stringToBytes(unescape(encodeURIComponent(str)));
-	},
-
-	// Convert a byte array to a string
-	bytesToString: function (bytes) {
-		return decodeURIComponent(escape(Binary.bytesToString(bytes)));
-	}
-
-};
-
-// Binary encoding
-var Binary = charenc.Binary = {
-
-	// Convert a string to a byte array
-	stringToBytes: function (str) {
-		for (var bytes = [], i = 0; i < str.length; i++)
-			bytes.push(str.charCodeAt(i));
-		return bytes;
-	},
-
-	// Convert a byte array to a string
-	bytesToString: function (bytes) {
-		for (var str = [], i = 0; i < bytes.length; i++)
-			str.push(String.fromCharCode(bytes[i]));
-		return str.join("");
-	}
-
-};
-
-})();
+function sessionState() {
+  var d = chrome.extension.getBackgroundPage().data;
+  if (d) {
+    return d;
+  }
+  d = {};
+  chrome.extension.getBackgroundPage().data = d;
+  return d;
 }
 
-/* SHA-256 */
-
-(function(){
-
-// Shortcuts
-var C = Crypto,
-    util = C.util,
-    charenc = C.charenc,
-    UTF8 = charenc.UTF8,
-    Binary = charenc.Binary;
-
-// Constants
-var K = [ 0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
-          0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
-          0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
-          0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
-          0xE49B69C1, 0xEFBE4786, 0x0FC19DC6, 0x240CA1CC,
-          0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
-          0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7,
-          0xC6E00BF3, 0xD5A79147, 0x06CA6351, 0x14292967,
-          0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
-          0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85,
-          0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3,
-          0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
-          0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5,
-          0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
-          0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
-          0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2 ];
-
-// Public API
-var SHA256 = C.SHA256 = function (message, options) {
-	var digestbytes = util.wordsToBytes(SHA256._sha256(message));
-	return options && options.asBytes ? digestbytes :
-	       options && options.asString ? Binary.bytesToString(digestbytes) :
-	       util.bytesToHex(digestbytes);
-};
-
-// The core
-SHA256._sha256 = function (message) {
-
-	// Convert to byte array
-	if (message.constructor == String) message = UTF8.stringToBytes(message);
-	/* else, assume byte array already */
-
-	var m = util.bytesToWords(message),
-	    l = message.length * 8,
-	    H = [ 0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
-	          0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19 ],
-	    w = [],
-	    a, b, c, d, e, f, g, h, i, j,
-	    t1, t2;
-
-	// Padding
-	m[l >> 5] |= 0x80 << (24 - l % 32);
-	m[((l + 64 >> 9) << 4) + 15] = l;
-
-	for (var i = 0; i < m.length; i += 16) {
-
-		a = H[0];
-		b = H[1];
-		c = H[2];
-		d = H[3];
-		e = H[4];
-		f = H[5];
-		g = H[6];
-		h = H[7];
-
-		for (var j = 0; j < 64; j++) {
-
-			if (j < 16) w[j] = m[j + i];
-			else {
-
-				var gamma0x = w[j - 15],
-				    gamma1x = w[j - 2],
-				    gamma0  = ((gamma0x << 25) | (gamma0x >>>  7)) ^
-				              ((gamma0x << 14) | (gamma0x >>> 18)) ^
-				               (gamma0x >>> 3),
-				    gamma1  = ((gamma1x <<  15) | (gamma1x >>> 17)) ^
-				              ((gamma1x <<  13) | (gamma1x >>> 19)) ^
-				               (gamma1x >>> 10);
-
-				w[j] = gamma0 + (w[j - 7] >>> 0) +
-				       gamma1 + (w[j - 16] >>> 0);
-
-			}
-
-			var ch  = e & f ^ ~e & g,
-			    maj = a & b ^ a & c ^ b & c,
-			    sigma0 = ((a << 30) | (a >>>  2)) ^
-			             ((a << 19) | (a >>> 13)) ^
-			             ((a << 10) | (a >>> 22)),
-			    sigma1 = ((e << 26) | (e >>>  6)) ^
-			             ((e << 21) | (e >>> 11)) ^
-			             ((e <<  7) | (e >>> 25));
-
-
-			t1 = (h >>> 0) + sigma1 + ch + (K[j]) + (w[j] >>> 0);
-			t2 = sigma0 + maj;
-
-			h = g;
-			g = f;
-			f = e;
-			e = d + t1;
-			d = c;
-			c = b;
-			b = a;
-			a = t1 + t2;
-
-		}
-
-		H[0] += a;
-		H[1] += b;
-		H[2] += c;
-		H[3] += d;
-		H[4] += e;
-		H[5] += f;
-		H[6] += g;
-		H[7] += h;
-
-	}
-
-	return H;
-
-};
-
-// Package private blocksize
-SHA256._blocksize = 16;
-
-})();
-
-/* HMAC */
-
-(function(){
-
-// Shortcuts
-var C = Crypto,
-    util = C.util,
-    charenc = C.charenc,
-    UTF8 = charenc.UTF8,
-    Binary = charenc.Binary;
-
-C.HMAC = function (hasher, message, key, options) {
-
-	// Convert to byte arrays
-	if (message.constructor == String) message = UTF8.stringToBytes(message);
-	if (key.constructor == String) key = UTF8.stringToBytes(key);
-	/* else, assume byte arrays already */
-
-	// Allow arbitrary length keys
-	if (key.length > hasher._blocksize * 4)
-		key = hasher(key, { asBytes: true });
-
-	// XOR keys with pad constants
-	var okey = key.slice(0),
-	    ikey = key.slice(0);
-	for (var i = 0; i < hasher._blocksize * 4; i++) {
-		okey[i] ^= 0x5C;
-		ikey[i] ^= 0x36;
-	}
-
-	var hmacbytes = hasher(okey.concat(hasher(ikey.concat(message), { asBytes: true })), { asBytes: true });
-
-	return options && options.asBytes ? hmacbytes :
-	       options && options.asString ? Binary.bytesToString(hmacbytes) :
-	       util.bytesToHex(hmacbytes);
-
-};
-
-})();
-
-/* PBKDF2 with SHA-256 */
-
-(function(){
-
-// Shortcuts
-var C = Crypto,
-    util = C.util,
-    charenc = C.charenc,
-    UTF8 = charenc.UTF8,
-    Binary = charenc.Binary;
-
-C.PBKDF2 = function (password, salt, keylen, options) {
-
-	// Convert to byte arrays
-	if (password.constructor == String) password = UTF8.stringToBytes(password);
-	if (salt.constructor == String) salt = UTF8.stringToBytes(salt);
-	/* else, assume byte arrays already */
-
-	// Defaults
-	var hasher = options && options.hasher || C.SHA256,
-	    iterations = options && options.iterations || 1;
-
-	// Pseudo-random function
-	function PRF(password, salt) {
-		return C.HMAC(hasher, salt, password, { asBytes: true });
-	}
-
-	// Generate key
-	var derivedKeyBytes = [],
-	    blockindex = 1;
-	while (derivedKeyBytes.length < keylen) {
-		var block = PRF(password, salt.concat(util.wordsToBytes([blockindex])));
-		for (var u = block, i = 1; i < iterations; i++) {
-			u = PRF(password, u);
-			for (var j = 0; j < block.length; j++) block[j] ^= u[j];
-		}
-		derivedKeyBytes = derivedKeyBytes.concat(block);
-		blockindex++;
-	}
-
-	// Truncate excess bytes
-	derivedKeyBytes.length = keylen;
-
-	return options && options.asBytes ? derivedKeyBytes :
-	       options && options.asString ? Binary.bytesToString(derivedKeyBytes) :
-	       util.bytesToHex(derivedKeyBytes);
-
-};
-
-})();
-
-// **** Password generation ****
+// Return a checksum that we can use to try to catch typoes.
+function partialHashMasterPassword(password, salt) {
+  // CRC32
+  s = String(password+salt);
+  var polynomial = 0x04C11DB7,
+      initialValue = 0xFFFFFFF,
+      finalXORValue = 0xFFFFFFFF,
+      crc = initialValue,
+      table = [], i, j, c;
+ 
+  function reverse(x, n) {
+    var b = 0;
+    while (n) {
+      b = b * 2 + x % 2;
+      x /= 2;
+      x -= x % 1;
+      n--;
+    }
+    return b;
+  }
+ 
+  for (i = 255; i >= 0; i--) {
+    c = reverse(i, 32);
+ 
+    for (j = 0; j < 8; j++) {
+      c = ((c * 2) ^ (((c >>> 31) % 2) * polynomial)) >>> 0;
+    }
+ 
+    table[i] = reverse(c, 32);
+  }
+ 
+  for (i = 0; i < s.length; i++) {
+    c = s.charCodeAt(i);
+    if (c > 255) {
+      throw new RangeError();
+    }
+    j = (crc % 256) ^ c;
+    crc = ((crc / 256) ^ table[j]) >>> 0;
+  }
+ 
+  // Cut in half, to REALLY make it low entropy. ;) 
+  return btoa((crc ^ finalXORValue) >>> 0).substr(0,6);
+}
 
 function makePassword() {
-	var secret = document.getElementById('secret').value;
-	var username = document.getElementById('username').value;
-	var url = document.getElementById('url').value;
-	var length = document.getElementById('length').value;
-    var salt = username + '@' + url;
- 
-	return Crypto.util.bytesToBase64(Crypto.PBKDF2(secret, salt, 32, { iterations: 5000, asBytes: true })).substring(0,length);
+    var password = document.getElementById('secret').value;
+    var username = document.getElementById('username').value;
+    var url = document.getElementById('url').value;
+    var length = document.getElementById('length').value;
+    var salt = document.getElementById('salt').value;
+
+    // Save settings.
+    chrome.storage.sync.set({'username':username,
+                             'salt':salt,
+                             'length':length}, function(){});
+    // Cache password.
+    sessionState()['secret'] = password;
+    var r = scrypt.crypto_scrypt(
+      scrypt.encode_utf8(password),
+      // TODO: is this a safe way to generate salt?
+      scrypt.encode_utf8(secret + url + username),
+      // Kind of a hack. Generate 2x length because we have to strip out the
+      // non-base62 base64 chars (so to speak).
+      32768, 8, 1, 2*length);
+    return btoa(String.fromCharCode.apply(null, r))
+      .replace(/[^A-Za-z0-9]/gm, '')
+      .substr(0, length);
 }
 
- 
 function toggleDiv(id)
 {
-	var infoStyle = document.getElementById(id).style;
-	if (infoStyle.display == "block")
-		infoStyle.display = "none";
-	else
-		infoStyle.display = "block";
+    var infoStyle = document.getElementById(id).style;
+    if (infoStyle.display == "block")
+        infoStyle.display = "none";
+    else
+        infoStyle.display = "block";
 }
- 
+
 function showDiv(id)
 {
-	var infoStyle = document.getElementById(id).style;
-	infoStyle.display = "block";
+    var infoStyle = document.getElementById(id).style;
+    infoStyle.display = "block";
 }
- 
+
 function hideDiv(id)
 {
-	var infoStyle = document.getElementById(id).style;
-	infoStyle.display = "none";
+    var infoStyle = document.getElementById(id).style;
+    infoStyle.display = "none";
 }
 
 function getHostname(str) {
     if (str == null || str == undefined)
         return "";
-	var re = new RegExp('^(?:f|ht)tp(?:s)?\://(?:www.)?([^/]+)', 'im');
-	var match = str.match(re);
+    var re = new RegExp('^(?:f|ht)tp(?:s)?\://(?:www.)?([^/]+)', 'im');
+    var match = str.match(re);
     if (match != null && match.length > 0)
         return match[1].toString();
     else
@@ -441,17 +118,33 @@ function generatePassword(event)
 {
     showPleaseWait();
     setTimeout(function() {
-        document.querySelector("#result-box").innerHTML = 
-            '<p class="centered">Your password <span class="gray">(copy and paste it)</span>:<br>'+
-            '<input type="text" spellcheck="false" class="centered" id="result" value=""></p>'+
-            '<div id="warning">Secret should contain at least 16 characters for better security.</div>';
-        var result_field = document.getElementById('result');
-        result_field.value = makePassword();
-        if (document.querySelector('#secret').value.length < 16)
-            showDiv('warning');
-        else
-            hideDiv('warning');
-        result_field.select();
+      var masterHash = sessionState()['master_hash'];
+      var secretElement = document.querySelector('#secret');
+      var secret = secretElement.value;
+      var warning = '';
+
+      newHash = partialHashMasterPassword(secret, document.getElementById('salt').value);
+      if (masterHash && masterHash != '' &&
+        newHash != masterHash) {
+        // Hash mismatch--typoed master password!
+        warning = '<div id="warning">Master password hash mismatch! Did you typo?</div>';
+      } else if (secret.length < 8) {
+        warning = '<div id="warning">Secret should contain at least 8 characters for better security.</div>';
+      }
+      // Set the master hash to the new hash, anyway.
+      chrome.storage.sync.set({'master_hash':newHash}, function(){});
+      sessionState()['master_hash'] = newHash;
+      document.querySelector("#result-box").innerHTML = 
+      '<p class="centered">Your password <span class="gray">(copy and paste it)</span>:<br>'+
+      '<input type="text" spellcheck="false" class="centered" id="result" value=""></p>'+
+      warning;
+      var result_field = document.getElementById('result');
+      result_field.value = makePassword();
+      if (warning)
+        showDiv('warning');
+      else
+        hideDiv('warning');
+      result_field.select();
     }, 60);
     event.preventDefault();
     event.stopPropagation();
@@ -503,13 +196,25 @@ function colorPasswordField() {
     secretElement.style.color = color;
 }
 
+function showHideOptions() {
+  sessionState()['options_shown'] = !sessionState()['options_shown'];
+  if (sessionState()['options_shown']) {
+    showDiv('options');
+  } else {
+    hideDiv('options');
+  }
+}
 
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('#main-form').addEventListener('submit', fillPassword);
+  if (!sessionState()['options_shown']) {
+    hideDiv("options");
+  }
+  document.querySelector('#main-form').addEventListener('keydown', function(e){ if (e.which == 13) { fillPassword(); }});
   document.querySelector('#show-button').addEventListener('click', generatePassword);
   document.querySelector('#fill-button').addEventListener('click', fillPassword);
-  document.querySelector('#secret').addEventListener('keydown', colorPasswordField);
-  document.querySelector('#secret').addEventListener('input', colorPasswordField);
+  document.querySelector('#options-button').addEventListener('click', showHideOptions);
+  document.querySelector('#secret').addEventListener('keydown', colorPasswordField());
+  document.querySelector('#secret').addEventListener('input', colorPasswordField());
 
   // Put website URL into box.
   chrome.tabs.getCurrent(function(tab) {
@@ -517,6 +222,17 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('#url').value = getSignificantDomain(getHostname(tabs[0].url));
       });
   });
+
+  // Fill settings.
+  chrome.storage.sync.get({'username':'', 'length':'16', 'salt':'', 'master_hash':''}, function(items){
+    document.getElementById('username').value = items.username;
+    document.getElementById('salt').value = items.salt;
+    document.getElementById('length').value = items.length;
+    sessionState()['master_hash'] = items.master_hash;
+  });
+  // Get cached password.
+  secret = sessionState()['secret'];
+  document.getElementById('secret').value = secret == null ? '' : secret;
 
   document.querySelector('#secret').focus();
 });
